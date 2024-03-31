5dc10b652ef5880de321596082098db8fb2ec274
Ardi S <ardisa2711@gmail.com>
2024-03-31 14:39:14 +0800

    penambahan fungsi tombol selesai dan belum selesai di baca

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
-  // Fungsi ketika checkbox selesai dibaca di klik
-  const checkbox = document.getElementById('cek-buku-selesai');
-  const statusBuku = document.getElementById('status-buku');
+document.addEventListener("DOMContentLoaded", function () {
+  // Fungsi ketika checkbox di klik
+  const checkbox = document.getElementById("cek-buku-selesai");
+  const statusBuku = document.getElementById("status-buku");
 
-  checkbox.addEventListener('change', function() {
+  checkbox.addEventListener("change", function () {
     if (checkbox.checked) {
-      statusBuku.innerText = 'Selesai Dibaca';
+      statusBuku.innerText = "Selesai Dibaca";
     } else {
-      statusBuku.innerText = 'Belum Selesai Dibaca';
+      statusBuku.innerText = "Belum Selesai Dibaca";
     }
   });
 
@@ -16,10 +16,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // Render buku
   function renderBuku() {
-    const listBukuBelumSelesaiDibaca = document.getElementById('list-buku-belum-selesai-dibaca');
-    const listBukuSelesaiDibaca = document.getElementById('list-buku-selesai-dibaca');
-    listBukuBelumSelesaiDibaca.innerHTML = '';
-    listBukuSelesaiDibaca.innerHTML = '';
+    const listBukuBelumSelesaiDibaca = document.getElementById("list-buku-belum-selesai-dibaca");
+    const listBukuSelesaiDibaca = document.getElementById("list-buku-selesai-dibaca");
+    listBukuBelumSelesaiDibaca.innerHTML = "";
+    listBukuSelesaiDibaca.innerHTML = "";
 
     for (const bukuItem of buku) {
       const infoBuku = buatBuku(bukuItem);
@@ -27,14 +27,14 @@ document.addEventListener('DOMContentLoaded', function() {
         listBukuSelesaiDibaca.appendChild(infoBuku);
       } else {
         listBukuBelumSelesaiDibaca.appendChild(infoBuku);
-      };
+      }
     }
   }
 
-  // Tambahkan buku baru
-  const inputBuku = document.getElementById('input-buku');
+  // Tambah buku
+  const inputBuku = document.getElementById("input-buku");
 
-  inputBuku.addEventListener('submit', function(event) {
+  inputBuku.addEventListener("submit", function (event) {
     event.preventDefault();
     tambahBuku();
     renderBuku();
@@ -42,10 +42,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // Fungsi tambah buku
   function tambahBuku() {
-    const judulBuku = document.getElementById('judul-buku').value;
-    const penulisBuku = document.getElementById('penulis-buku').value;
-    const tahunBuku = document.getElementById('tahun-buku').value;
-    const selesaiBaca = document.getElementById('cek-buku-selesai').checked;
+    const judulBuku = document.getElementById("judul-buku").value;
+    const penulisBuku = document.getElementById("penulis-buku").value;
+    const tahunBuku = document.getElementById("tahun-buku").value;
+    const selesaiBaca = document.getElementById("cek-buku-selesai").checked;
 
     // Buat id buku
     function buatIdBuku() {
@@ -60,56 +60,155 @@ document.addEventListener('DOMContentLoaded', function() {
         judul,
         penulis,
         tahun,
-        selesai
-      }
+        selesai,
+      };
     }
 
-    // Tambahkan buku ke dalam array
-    const objekBuku = buatObjekBuku(idBuku, judulBuku, penulisBuku, tahunBuku, selesaiBaca);
+    // Tambah buku ke dalam array
+    const objekBuku = buatObjekBuku(
+      idBuku,
+      judulBuku,
+      penulisBuku,
+      tahunBuku,
+      selesaiBaca
+    );
     buku.push(objekBuku);
 
     // Trigger event render-buku
-    const RENDER_EVENT = 'render-buku';
+    const RENDER_EVENT = "render-buku";
     document.dispatchEvent(new Event(RENDER_EVENT));
   }
 
-  // Listener untuk event render-buku
-  document.addEventListener('render-buku', renderBuku);
+  // Listener event render-buku
+  document.addEventListener("render-buku", renderBuku);
 
   // Fungsi untuk membuat elemen buku
-  function buatBuku(objekBuku){
-    const infoBuku = document.createElement('div');
-    infoBuku.classList.add('info-buku');
+  function buatBuku(objekBuku) {
+    const infoBuku = document.createElement("div");
+    infoBuku.classList.add("info-buku");
 
-    const judulBuku = document.createElement('h3');
-    judulBuku.classList.add('judul-buku');
+    const judulBuku = document.createElement("h3");
+    judulBuku.classList.add("judul-buku");
     judulBuku.innerText = objekBuku.judul;
     infoBuku.appendChild(judulBuku);
 
-    const penulisBuku = document.createElement('p');
-    penulisBuku.classList.add('penulis-buku');
+    const penulisBuku = document.createElement("p");
+    penulisBuku.classList.add("penulis-buku");
     penulisBuku.innerText = objekBuku.penulis;
     infoBuku.appendChild(penulisBuku);
 
-    const tahunBuku = document.createElement('p');
-    tahunBuku.classList.add('tahun-buku');
+    const tahunBuku = document.createElement("p");
+    tahunBuku.classList.add("tahun-buku");
     tahunBuku.innerText = objekBuku.tahun;
     infoBuku.appendChild(tahunBuku);
 
-    const aksiBuku = document.createElement('div');
-    aksiBuku.classList.add('aksi-buku');
+    const aksiBuku = document.createElement("div");
+    aksiBuku.classList.add("aksi-buku");
     infoBuku.appendChild(aksiBuku);
 
-    const selesaiBaca = document.createElement('button');
-    selesaiBaca.classList.add('selesai-dibaca');
-    selesaiBaca.innerText = objekBuku.selesai ? 'Belum Selesai Dibaca' : 'Selesai Dibaca'; 
-    aksiBuku.appendChild(selesaiBaca);
+    if (objekBuku.selesai) {
+      const selesaiBaca = document.createElement("button");
+      selesaiBaca.classList.add("selesai-dibaca");
+      selesaiBaca.innerText = "Belum Selesai Dibaca";
+      aksiBuku.appendChild(selesaiBaca);
 
-    const hapusBuku = document.createElement('button');
-    hapusBuku.classList.add('hapus-buku');
-    hapusBuku.innerText = 'Hapus Buku';
-    aksiBuku.appendChild(hapusBuku);
+      selesaiBaca.addEventListener("click", function () {
+        tambahBukuKeBelumSelesaiDibaca(objekBuku.id);
+      });
 
+      const hapusBuku = document.createElement("button");
+      hapusBuku.classList.add("hapus-buku");
+      hapusBuku.innerText = "Hapus Buku";
+      aksiBuku.appendChild(hapusBuku);
+
+      hapusBuku.addEventListener("click", function () {
+        hapusBukuDariDaftar(objekBuku.id);
+      });
+    } else {
+      const selesaiBaca = document.createElement("button");
+      selesaiBaca.classList.add("selesai-dibaca");
+      selesaiBaca.innerText = "Selesai Dibaca";
+      aksiBuku.appendChild(selesaiBaca);
+
+      selesaiBaca.addEventListener("click", function () {
+        tambahBukuKeSelesaiDibaca(objekBuku.id);
+      });
+
+      const hapusBuku = document.createElement("button");
+      hapusBuku.classList.add("hapus-buku");
+      hapusBuku.innerText = "Hapus Buku";
+      aksiBuku.appendChild(hapusBuku);
+
+      hapusBuku.addEventListener("click", function () {
+        hapusBukuDariDaftar(objekBuku.id);
+      });
+    }
     return infoBuku;
   }
+
+  // Fungsi untuk menambahkan buku ke dalam daftar selesai dibaca
+  function tambahBukuKeSelesaiDibaca(idBuku) {
+    for (const bukuItem of buku) {
+      if (bukuItem.id === idBuku) {
+        bukuItem.selesai = true;
+      }
+    }
+    const RENDER_EVENT = "render-buku";
+    document.dispatchEvent(new Event(RENDER_EVENT));
+  }
+
+  // Fungsi untuk menambahkan buku ke dalam daftar belum selesai dibaca
+  function tambahBukuKeBelumSelesaiDibaca(idBuku) {
+    for (const bukuItem of buku) {
+      if (bukuItem.id === idBuku) {
+        bukuItem.selesai = false;
+      }
+    }
+    const RENDER_EVENT = "render-buku";
+    document.dispatchEvent(new Event(RENDER_EVENT));
+  }
+
+  // Fungsi untuk menghapus buku dari daftar
+  function hapusBukuDariDaftar(idBuku) {
+    for (let i = 0; i < buku.length; i++) {
+      if (buku[i].id === idBuku) {
+        buku.splice(i, 1);
+      }
+    }
+    const RENDER_EVENT = "render-buku";
+    document.dispatchEvent(new Event(RENDER_EVENT));
+  }
+
+  // Fungsi untuk pencarian buku
+  const formBuku = document.getElementById("cari-buku");
+  formBuku.addEventListener("input", function () {
+    event.preventDefault();
+    cariBuku();
+  });
+
+  function cariBuku() {
+    const kataKunci = document.getElementById("cari-judul").value.toLowerCase();
+    const hasilPencarian = buku.filter((buku) =>
+      buku.judul.toLowerCase().includes(kataKunci)
+    );
+  
+    const listBukuBelumSelesaiDibaca = document.getElementById(
+      "list-buku-belum-selesai-dibaca"
+    );
+    const listBukuSelesaiDibaca = document.getElementById(
+      "list-buku-selesai-dibaca"
+    );
+  
+    listBukuBelumSelesaiDibaca.innerHTML = "";
+    listBukuSelesaiDibaca.innerHTML = "";
+  
+    for (const bukuItem of hasilPencarian) {
+      const infoBuku = buatBuku(bukuItem);
+      if (bukuItem.selesai) {
+        listBukuSelesaiDibaca.appendChild(infoBuku);
+      } else {
+        listBukuBelumSelesaiDibaca.appendChild(infoBuku);
+      }
+    }
+  } 
 });
